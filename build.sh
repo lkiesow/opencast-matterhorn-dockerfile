0076c9a43ef4aabbf65db243b7e7cde5c2097760
Lars Kiesow <lkiesow@uos.de>
2014-11-25 16:40:13 +0100

    Fixed build.sh for build without password
    Signed-off-by: Lars Kiesow <lkiesow@uos.de>

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Switch to script dir:
+cd $(dirname $(readlink -f $0))
+
 echo '
 Opencast Matterhorn Dockerfile
 
@@ -15,20 +18,25 @@ echo -n 'Password: '
 read -s PASSWD
 echo
 
+docker_build() {
+	docker build -t opencast-matterhorn16 . && \
+		echo Successfully built Matterhorn Docker image && \
+		echo Start Matterhorn by running && \
+		echo '  docker run -d --name=matterhorn16 -p 8080:8080 opencast-matterhorn16'
+}
+
 if [[ $USER == "" ]] || [[ $PASSWD == "" ]]
 then
 	echo No credentials used. This will only work if you have direct access to
 	echo the Matterhorn RPM repository.
+	docker_build
 else
 	echo Building Docker image. This might take a while
 	cp matterhorn.repo matterhorn.repo.1
 	cp matterhorn-testing.repo matterhorn-testing.repo.1
 	sed -i "s/repo.virt/${USER}:${PASSWD}@repo.virt/" matterhorn.repo
 	sed -i "s/repo.virt/${USER}:${PASSWD}@repo.virt/" matterhorn-testing.repo
-	docker build -t opencast-matterhorn16 . && \
-		echo Successfully built Matterhorn Docker image && \
-		echo Start Matterhorn by running && \
-		echo '  docker run -d --name=matterhorn16 -p 8080:8080 opencast-matterhorn16'
+	docker_build
 	mv matterhorn.repo.1 matterhorn.repo
 	mv matterhorn-testing.repo.1 matterhorn-testing.repo
 fi
